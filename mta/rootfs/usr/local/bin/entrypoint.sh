79f2e8fedd6d3991beca6c0a0d273543a662d87c
Aaron Chang <aaronchn@live.com>
2020-09-03 23:31:35 +0800

    Add recipient-access conf in MTA

--- mta/rootfs/usr/local/bin/entrypoint.sh
+++ mta/rootfs/usr/local/bin/entrypoint.sh
@@ -16,9 +16,10 @@ fi
 
 dockerize \
   -template /etc/postfix/ldap-domains.cf.templ:/etc/postfix/ldap-domains.cf \
-  -template /etc/postfix/ldap-users.cf.templ:/etc/postfix/ldap-user.cf \
+  -template /etc/postfix/ldap-users.cf.templ:/etc/postfix/ldap-users.cf \
   -template /etc/postfix/ldap-groups.cf.templ:/etc/postfix/ldap-groups.cf \
   -template /etc/postfix/ldap-aliases.cf.templ:/etc/postfix/ldap-aliases.cf \
+  -template /etc/postfix/ldap-recipient-access.cf.templ:/etc/postfix/ldap-recipient-access.cf \
   -wait tcp://${MDA_HOST}:2003 \
   -wait tcp://${RSPAMD_HOST}:11332 \
   -wait file://${SSL_CERT} \
